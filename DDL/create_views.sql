72ffb64240b2a74b6c420e4d757bea07ac77c38f
Petyo <petyozh@yahoo.com>
2021-03-13 20:15:54 +0100

    Update create_views.sql

--- DDL/create_views.sql
+++ DDL/create_views.sql
@@ -1,2 +1,7 @@
 -- Create views.
 -- Goes after create_tables.
+
+CREATE VIEW `v_Courses` AS
+SELECT Course.Name, Course.Length, Course.Difficulty, Category.Name 
+FROM Course INNER JOIN Category
+ON Course.Category_ID = Category.Category_ID
